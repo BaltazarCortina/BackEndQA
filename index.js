65df97c5cccd7598c2ac20c2ae7ef5d3f33dbf2c
Baltazar Cortina <baltazarcortina@gmail.com>
2021-04-19 13:29:55 -0300

    Added verifications to check if an user exists and has valid credentials, added error messages.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.use(express.json());
 
-users = []
+var users = []
 
 app.get('/', (req, res) => {
 	res.send('Holaa!!');
@@ -15,22 +15,49 @@ app.get('/', (req, res) => {
 
 app.post('/register', (req, res) => {
 	// Chequear si el usuario ya existe!
-	const newUser = {
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
+	let userExist = false;
+	for (let i = 0; i < users.length; i++) {
+		if (req.body.email === users[i].email) {
+			userExist =true
+		}
 	}
+	if (!userExist) {
+		const newUser = {
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password
+		}
 
-	users.push(newUser);
-	console.log(users);
-	res.send({ result: 'Success' });
+		users.push(newUser);
+		console.log('--Created new user--\nActive users:');
+		console.log(users);
+		res.send({ result: 'Success' });	
+	} else {
+		console.log('--User already exists--');
+		res.status(400).send({ result: 'Failed' });
+	}
+	
 })
 
 app.put('/login', (req, res) => {
-	console.log('New login:')
-	console.log('-email: ' + req.body.email);
-	console.log('-password: ' + req.body.password);
-	res.send({ result: 'Success' });
+	let validUser = false;
+	for (let i = 0; i < users.length; i++) {
+		if (req.body.email === users[i].email) {
+			if (req.body.password === users[i].password) {
+				validUser = true;
+			}
+		}
+	}
+	if (validUser) {
+		console.log('--New login--\nUser:')
+		console.log('-email: ' + req.body.email);
+		console.log('-password: ' + req.body.password);
+		res.send({ result: 'Success' });	
+	} else {
+		console.log('--Invalid User--');
+		res.status(401).send({ result: 'Failed' });
+	}
+	
 })
 
 app.listen(port, () => {
