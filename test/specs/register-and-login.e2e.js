14ea42bbc0028337c44a1a6d026477c53bbbed14
Baltazar Cortina <baltazarcortina@gmail.com>
2021-05-10 17:51:59 -0300

    Added test to register a new account and log into it.

---
+++ test/specs/register-and-login.e2e.js
@@ -0,0 +1,31 @@
+const RegisterPage = require('../pageobjects/register.page');
+const LoginPage = require('../pageobjects/login.page');
+
+/*
+ * The purpose of this test is to validate that you can log into an account that was just registered.
+ * The test is in a separate file to be able to run it independently from the rest of the Register and Login tests, 
+ * beacuse there's not a database to store the user's information.
+*/
+
+describe('Testing registering a new account and loging into it:', () => {
+    it ('Should allow registration of a new account with valid inputs', () => {
+        RegisterPage.open();
+        RegisterPage.register('New User','newuser@email.com', 'user1234', 'user1234');
+
+        expect(RegisterPage.registerResult).toHaveText('Successfully created user!');
+    })
+    
+    it ('Should allow access to the user just created', () => {
+        LoginPage.open();
+        LoginPage.login('newuser@email.com', 'user1234');
+
+        expect(LoginPage.successfullLogin).toHaveText('Welcome New');
+    })
+
+    it ('Should deny access to the user just created with wrong credentials', () => {
+        LoginPage.open();
+        LoginPage.login('newuser@email.com', 'user4321');
+
+        expect(LoginPage.loginResult).toHaveText('The password is incorrect!');
+    })
+})
